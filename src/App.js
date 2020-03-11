79416b83b557cadd265b40ac794c1089776e70ee
Giovani de Biagi Alves <giovani.alves@upx.com>
2020-03-11 18:25:23 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Main from './layout/Main';
+import { PrivateRoute } from './components/PrivateRoute';
+import { Login } from './components/Login';
+import { Register } from './components/Register';
+import { ForgotPassword } from './components/ForgotPassword';
+import { Route, Switch } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+    <Switch>
+        <Route path="/" exact component={Login} />
+        <Route path="/forgotPassword" exact component={ForgotPassword} />
+        <Route path="/register" exact component={Register} />
+        <PrivateRoute>
+            <Main />
+        </PrivateRoute>
+    </Switch>
+
+);
 
 export default App;
